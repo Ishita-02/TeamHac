24e8efb48283bba4bffaa6e26cf368fa99eba0c5
Ishita-02 <88130678+Ishita-02@users.noreply.github.com>
2024-01-20 01:30:04 +0530

    commit

---
+++ client/src/Components/Signup.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import './Signup.css';
+
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSignup = async() => {
+        const response = await fetch('http://localhost:3000/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+        if(data.token) {
+            localStorage.setItem("token", data.token)
+            const navigate = useNavigate();
+            navigate("/login");
+        }
+        else {
+            alert("Error while signing up");
+        }
+    };
+
+    return (
+        <div style={{justifyContent: "center", display: "flex", width: "100%"}}>
+            <div>
+                <h2>Signup</h2>
+                <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Username' />
+                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+                Already signed up? <Link to="/login">Login</Link>
+                <button onClick={handleSignup}>Signup</button>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
