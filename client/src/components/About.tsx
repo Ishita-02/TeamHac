e626c59b003c98294d150d13aaa707c3f30f1d85
Ishita-02 <88130678+Ishita-02@users.noreply.github.com>
2024-02-04 23:44:28 +0530

    Removed some files

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -25,14 +25,6 @@ export default function About() {
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Whether you're an individual seeking a team or a team searching for the missing piece, we bring talented minds together to turn ideas into reality. 
             </p>
-            {/* <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/signup"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Signup
-              </a>
-            </div> */}
           </div>
         </div>
         <div
